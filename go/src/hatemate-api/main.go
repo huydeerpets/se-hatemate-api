7cb4627a8b9733461531febbdcb42f9a30f528ed
Arnold <arnold777armageddon@gmail.com>
2018-05-22 08:32:06 +0700

    configure psql server but still error

--- go/src/hatemate-api/main.go
+++ go/src/hatemate-api/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	_ "fmt"
 	_ "hatemate-api/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
 )
 
+func init() {
+	orm.RegisterDriver("postgres", orm.DRPostgres)
+	orm.RegisterDataBase(
+		"default",
+		"postgres",
+		"user=xxx password=xxx host=127.0.0.1 port=5432 dbname=gotest sslmode=disable")
+
+	orm.RunSyncdb("default", false, true)
+}
+
 func main() {
+	orm.Debug = true
+
+	o := orm.NewOrm()
+	o.Using("default")
+
+	// TestUser = models.hatemateusers.HateMateUser
+	// TestUser.ID = 0
+	// TestUser.Email = "dummy@dummy.com"
+	// TestUser.Password = "dummy"
+	// TestUser.FirstName = "Fon"
+	// TestUser.LastName = "Magenus"
+	// TestUser.Gender = "Male"
+
+	// id, err := o.Insert(TestUser)
+	// fmt.Printf("ID: %d, ERR: %v\n", id, err)
 	beego.Run()
 }
--- go/src/hatemate-api/models/hatemateusers.go
+++ go/src/hatemate-api/models/hatemateusers.go
@@ -1,7 +1,18 @@
 package hatemateusers
 
-type person struct {
+import (
+	"github.com/astaxie/beego/orm"
+)
+
+type HateMateUser struct {
+	ID        int    `json:"user_id"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Gender    string `json:"gender"`
+}
+
+func init() {
+	orm.RegisterModel(new(HateMateUser))
 }
