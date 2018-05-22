8bfcd6078e2130347922695c27cfffc823399eeb
Muhammad Ananda Ryanizar <anandar.ryanizar@gmail.com>
2018-05-22 14:53:10 +0700

    test

---
+++ go/src/hatemate-api/controllers/user.go
@@ -0,0 +1,62 @@
+package controllers
+
+import (
+	"encoding/json"
+	"hatemate-api/models"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+)
+
+// UserController test
+type UserController struct {
+	beego.Controller
+}
+
+// Get user
+func (uc *UserController) Get() {
+	var user models.HateMateUser
+	json.Unmarshal(uc.Ctx.Input.RequestBody, &user)
+
+	o := orm.NewOrm()
+	o.Using("default")
+
+	err := o.Read(&user, "Email", "Password")
+
+	if err == orm.ErrNoRows {
+		uc.Ctx.Output.SetStatus(404)
+		uc.Ctx.Output.Body([]byte("User not found"))
+		return
+	}
+
+	uc.Data["json"] = user
+	uc.ServeJSON()
+}
+
+// Insert user
+func (uc *UserController) Insert() {
+	var user models.HateMateUser
+	json.Unmarshal(uc.Ctx.Input.RequestBody, &user)
+
+	o := orm.NewOrm()
+	o.Using("default")
+
+	err := o.Read(&user, "email")
+
+	if err == nil {
+		uc.Ctx.Output.SetStatus(400)
+		uc.Ctx.Output.Body([]byte("User already exists"))
+		return
+	}
+
+	id, err := o.Insert(&user)
+
+	if err != nil {
+		uc.Ctx.Output.SetStatus(400)
+		uc.Ctx.Output.Body([]byte("Error inserting user"))
+		return
+	}
+
+	uc.Data["json"] = id
+	uc.ServeJSON()
+}
--- go/src/hatemate-api/routers/router.go
+++ go/src/hatemate-api/routers/router.go
@@ -8,4 +8,6 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
+	beego.Router("/GetUser", &controllers.UserController{}, "*:Get")
+	beego.Router("/RegisterUser", &controllers.UserController{}, "*:Insert")
 }
